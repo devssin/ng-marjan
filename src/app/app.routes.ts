a401fdfbaf856d0613e867f73bdb082f14024676
devssin <yaaynealhayate@positeams.com>
2023-12-05 14:27:55 +0100

    create first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'admin',
+        component: AdminComponent
+    },
+    {
+        path: 'admin-dashboard',
+        component: AdminDashboardComponent
+    }
+];
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+    "./node_modules/flowbite/**/*.js"
+
+  ],
+  theme: {
+    colors: {
+      "primary": "#0055a6",
+      "secondary": "#ffe600"
+    },
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+
+  ],
+}
