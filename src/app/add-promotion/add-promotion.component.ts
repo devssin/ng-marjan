3bc2b5917482430d239ff8907329c79ec6f025b9
devssin <yaaynealhayate@positeams.com>
2023-12-07 09:06:03 +0100

    add promotion page

---
+++ src/app/add-promotion/add-promotion.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { AdminSidebarComponent } from '../inc/admin-sidebar/admin-sidebar.component';
+
+@Component({
+  selector: 'app-add-promotion',
+  standalone: true,
+  imports: [AdminSidebarComponent],
+  templateUrl: './add-promotion.component.html',
+  styleUrl: './add-promotion.component.css'
+})
+export class AddPromotionComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { AddPromotionComponent } from './add-promotion/add-promotion.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path: 'admin-dashboard',
         component: AdminDashboardComponent
+    },
+    {
+        path: "add-promotion",
+        component: AddPromotionComponent
     }
 ];
